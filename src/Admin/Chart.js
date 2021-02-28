8b25a216d39f8c19ac08ce22ef2cc9d13105c25e
Aathik Ahamed <aathikahamed@gmail.com>
2021-02-28 21:59:44 +0000

    Initial Commit

---
+++ src/Admin/Chart.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import Title from "./Title";
+
+const data = {
+  labels: [
+    "10/04/2018",
+    "10/05/2018",
+    "10/09/2018",
+    "10/10/2018",
+    "10/11/2018",
+    "10/12/2018",
+    "10/13/2018",
+    "10/14/2018",
+    "10/15/2018",
+  ],
+  datasets: [
+    {
+      label: "$ Revenue",
+      data: [22, 19, 27, 23, 22, 24, 17, 25, 19],
+      fill: false,
+      borderColor: "#3f51b5",
+    },
+  ],
+};
+
+export default function Chart() {
+  return (
+    <React.Fragment>
+      <Title>This month Income</Title>
+      <div style={{ height: "100%" }}>
+        <Line data={data} options={{ maintainAspectRatio: false }} />
+      </div>
+    </React.Fragment>
+  );
+}
---
+++ src/Admin/listItems.js
@@ -0,0 +1,72 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PeopleIcon from "@material-ui/icons/People";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+
+export const mainListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIndIcon />
+      </ListItemIcon>
+      <ListItemText primary="Students" />
+    </ListItem>
+
+    <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Staff" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AttachMoneyIcon />
+      </ListItemIcon>
+      <ListItemText primary="Fees" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Exams" />
+    </ListItem>
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    <ListSubheader inset>Saved reports</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItem>
+  </div>
+);
